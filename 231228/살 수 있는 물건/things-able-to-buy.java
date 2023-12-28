c41fc548cc7a8293ba59ebb751cbabd7c52b87f3
ssh5340 <113916315+ssh5340@users.noreply.github.com>
2023-12-28 12:55:48 +0900

    Title: 살 수 있는 물건, Time: 102ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231228/살 수 있는 물건/things-able-to-buy.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException, NumberFormatException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        String result = "";
+        if (n >= 1000) {
+            if (n >= 3000) {
+                result = "book";
+            } else {
+                result = "mask";
+            }
+        } else {
+            result = "no";
+        }
+        
+        bw.write(result);
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
