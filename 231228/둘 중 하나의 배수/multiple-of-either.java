72a9a87fe810fd6aeff216ec204d773ef4ec7680
ssh5340 <113916315+ssh5340@users.noreply.github.com>
2023-12-28 13:30:09 +0900

    Title: 둘 중 하나의 배수, Time: 178ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231228/둘 중 하나의 배수/multiple-of-either.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException, NumberFormatException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int a = Integer.parseInt(br.readLine());
+
+        if (a % 3 == 0 || a % 5 == 0) {
+            bw.write("1");
+        } else {
+            bw.write("0");
+        } 
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
