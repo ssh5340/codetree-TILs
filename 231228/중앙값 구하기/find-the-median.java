0ff60eff989048915ad349b05d30ff46ee0d691d
ssh5340 <113916315+ssh5340@users.noreply.github.com>
2023-12-28 13:37:48 +0900

    Title: 중앙값 구하기, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231228/중앙값 구하기/find-the-median.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException, NumberFormatException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input = br.readLine();
+        StringTokenizer st = new StringTokenizer(input);
+        int[] arr = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        bw.write(String.valueOf(arr[1]));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
