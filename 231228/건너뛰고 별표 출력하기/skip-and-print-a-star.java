f57fa029b1098251b95bb5a7b8e59d9621ff0dcd
ssh5340 <113916315+ssh5340@users.noreply.github.com>
2023-12-28 16:16:31 +0900

    Title: 건너뛰고 별표 출력하기, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231228/건너뛰고 별표 출력하기/skip-and-print-a-star.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException, NumberFormatException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 0; j < i; j++) {
+                bw.write("*");
+            }
+            bw.write("\n\n");
+        }
+
+        for (int i = n-1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                bw.write("*");
+            }
+            bw.write("\n\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
