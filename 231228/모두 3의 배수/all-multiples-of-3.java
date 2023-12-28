a3bae19fcbcf77cadf79ed98cf9023ed287083a5
ssh5340 <113916315+ssh5340@users.noreply.github.com>
2023-12-28 15:53:12 +0900

    Title: 모두 3의 배수, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

--- 231228/모두 3의 배수/all-multiples-of-3.java
+++ 231228/모두 3의 배수/all-multiples-of-3.java
@@ -14,7 +14,7 @@ public class Main {
         }
 
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] % 3 == 0) {
+            if (arr[i] % 3 != 0) {
                 result = "0";
                 break;
             }
