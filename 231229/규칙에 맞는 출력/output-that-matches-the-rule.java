1d6732d9ff6344df8f84ccb85dd2420fe1387f75
ssh5340 <113916315+ssh5340@users.noreply.github.com>
2023-12-29 10:33:32 +0900

    Title: 규칙에 맞는 출력, Time: 113ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 231229/규칙에 맞는 출력/output-that-matches-the-rule.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException, NumberFormatException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            for (int j = n - i; j < n + 1; j++) {
+                bw.write(j + " ");
+            }
+            bw.write("\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
