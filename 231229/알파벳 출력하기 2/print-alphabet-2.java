cd51bff57ddcfaaf06abff6e7477b599bebbff5c
ssh5340 <113916315+ssh5340@users.noreply.github.com>
2023-12-29 13:08:58 +0900

    Title: 알파벳 출력하기 2, Time: 92ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231229/알파벳 출력하기 2/print-alphabet-2.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException, NumberFormatException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int cnt = 65;
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                bw.write("  ");
+            }
+            for (int j = n - i; j > 0; j--) {
+                if (cnt == 90) {
+                    cnt = 65;
+                }
+                char result = (char)cnt;
+                bw.write(String.valueOf(result + " "));
+                cnt++;
+            }
+            bw.write("\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
