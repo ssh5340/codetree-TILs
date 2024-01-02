b34d071d32afd07562ad0d5fb9ac4b67b94db431
ssh5340 <113916315+ssh5340@users.noreply.github.com>
2024-01-02 12:48:05 +0900

    Title: 격자로 사각형 만들기, Time: 185ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240102/격자로 사각형 만들기/print-grid-in-rectangle.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException, NumberFormatException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            arr[0][i] = 1;
+            arr[i][0] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                arr[i][j] = arr[i - 1][j] + arr[i - 1][j - 1] + arr[i][j - 1];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                bw.write(arr[i][j] + " ");
+            }
+            bw.write("\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
